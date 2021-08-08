06ea9e28097b66de46681619afdbefcaf29d2b6d
Andrzej Polak <andrzejp2805@gmail.com>
2021-08-09 00:54:45 +0200

    Code refactoring

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 const span = document.querySelector(".days-left");
 
+function renderDaysLeft(daysLeft, hoursLeft) {
+  span.innerText = `${daysLeft} dni ${hoursLeft} godzin`;
+}
+
+function getDays(distance) {
+  return Math.floor(distance / (1000 * 60 * 60 * 24));
+}
+
+function getHours(distance) {
+  return Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+}
+
+function getWeddingDate() {
+  return new Date("Oct 14, 2022 15:00:00").getTime();
+}
+
+function getCurrentDate() {
+  return new Date().getTime();
+}
+
 function daysLeftToWedding(weddingDate) {
-  const now = new Date().getTime();
+  const now = getCurrentDate();
   const distance = weddingDate - now;
 
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  span.innerText = days;
+  const days = getDays(distance);
+  const hours = getHours(distance);
+  renderDaysLeft(days, hours)
 }
 
 function countdownToWedding() {
-  const weddingDate = new Date("Oct 14, 2022 15:00:00").getTime();
+  const weddingDate = getWeddingDate();
   daysLeftToWedding(weddingDate);
 }
-
 
 countdownToWedding();
